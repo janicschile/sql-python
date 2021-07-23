6afd9ffc2e860723801b13066319809e54f0f3c5
Oscar Guerrero G <oguerrerog@gmail.com>
2021-07-23 19:12:05 -0400

    first commit

--- comandos CRUD.sql
+++ comandos CRUD.sql
@@ -1,5 +1,6 @@
 use panel_de_usuarios;
-INSERT INTO usuarios ( username, password, email, validar_email, nombre, apellido, descripcion, tipo_usuario, created_at) values ( 'oguerrero', '123456', 'oguerrerog@gmail.com', 'oguerrerog@gmail.com', 'Oscar', 'Guerrero', 'Administrador Web', '0', NOW());
+INSERT INTO usuarios ( username, password, email, validar_email, nombre, apellido, descripcion, tipo_usuario, created_at) 
+values ( 'oguerrero', '123456', 'oguerrerog@gmail.com', 'oguerrerog@gmail.com', 'Oscar', 'Guerrero', 'Administrador Web', '0', NOW());
 SELECT * FROM usuarios WHERE id = 1;
 UPDATE usuarios SET descripcion = 'Super Administrador Web', updated_at = NOW() WHERE id = 1;
 DELETE FROM usuarios WHERE id = 1;
