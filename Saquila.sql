b8cd8afb4e8d4c508d8bf59886f18265cc0b718c
Oscar Guerrero G <oguerrerog@gmail.com>
2021-07-23 18:17:56 -0400

    first commit

--- Saquila.sql
+++ Saquila.sql
@@ -1,8 +1,10 @@
 1. ¿Qué consulta ejecutarías para obtener todos los clientes dentro de city_id = 312? 
 Su consulta debe devolver el nombre, apellido, correo electrónico y dirección del cliente.
 ///////////////////
+
 select first_name, last_name, email, address from customer join address ON address.address_id = customer.customer_id
 WHERE city_id = 312;
+
 ///////////////////
 
 
@@ -10,10 +12,12 @@ WHERE city_id = 312;
 Su consulta debe devolver el título de la película, la descripción, el año de estreno, la calificación, 
 las características especiales y el género (categoría).
 ///////////////////
+
 select film.film_id, title, description, release_year, rating, special_features, category.name from film
 join film_category ON film.film_id = film_category.film_id
 join category ON film_category.category_id = category.category_id
 where category.name = 'comedy';
+
 ///////////////////
 
 
@@ -23,10 +27,12 @@ where category.name = 'comedy';
 Su consulta debe devolver la identificación del actor, el nombre del actor, el título de la película, 
 la descripción y el año de lanzamiento.
 ///////////////////
+
 select actor.actor_id, CONCAT(first_name,' ', last_name) as actor_name, film.film_id, film.title, film.description, film.release_year from actor
 join film_actor ON  film_actor.actor_id = actor.actor_id
 join film ON film_actor.film_id = film.film_id 
 where actor.actor_id = 5;
+
 ///////////////////
 
 
@@ -34,9 +40,11 @@ where actor.actor_id = 5;
 4. ¿Qué consulta ejecutaría para obtener todos los clientes en store_id = 1 y dentro de estas ciudades (1, 42, 312 y 459)? 
 Su consulta debe devolver el nombre, apellido, correo electrónico y dirección del cliente.
 ///////////////////
+
 select * from customer
 join address ON customer.address_id = address.address_id 
 where store_id = 1 and address.city_id in (1,42,312,459);
+
 ///////////////////
 
 
@@ -46,10 +54,12 @@ where store_id = 1 and address.city_id in (1,42,312,459);
 Su consulta debe devolver el título de la película, la descripción, el año de lanzamiento, la calificación y la función especial. 
 Sugerencia: puede usar la función LIKE para obtener la parte 'detrás de escena'.
 ///////////////////
+
 select film.title, film.description, film.release_year, rating, special_features from actor
 join film_actor ON  film_actor.actor_id = actor.actor_id
 join film ON film_actor.film_id = film.film_id 
 where rating = 'g' and actor.actor_id = 15 and special_features like '%behind the scenes%';
+
 ///////////////////
 
 
@@ -58,10 +68,12 @@ where rating = 'g' and actor.actor_id = 15 and special_features like '%behind th
 6. ¿Qué consulta harías para obtener todos los actores que se unieron en el film_id = 369? 
 Su consulta debe devolver film_id, title, actor_id y actor_name.
 ///////////////////
+
 select film.film_id, film.title, actor.actor_id, CONCAT(first_name ,' ', last_name) as actor_name from actor
 join film_actor ON  film_actor.actor_id = actor.actor_id
 join film ON film_actor.film_id = film.film_id
 where film.film_id = 369;
+
 ///////////////////
 
 
@@ -70,10 +82,12 @@ where film.film_id = 369;
 Su consulta debe devolver el título de la película, la descripción, el año de estreno, la calificación, 
 las características especiales y el género (categoría).
 ///////////////////
+
 select film.film_id, film.title, film.description, film.release_year, film.rating, special_features, category.name as genero, rental_rate from film
 join film_category ON film.film_id = film_category.film_id
 join category ON film_category.category_id = category.category_id
 where category.name = 'drama' and film.rental_rate = 2.99;
+
 ///////////////////
 
 
@@ -82,11 +96,13 @@ where category.name = 'drama' and film.rental_rate = 2.99;
 Su consulta debe devolver el título de la película, la descripción, el año de estreno, la calificación, 
 las características especiales, el género (categoría) y el nombre y apellido del actor.
 ///////////////////
+
 select * from actor
 join film_actor ON  film_actor.actor_id = actor.actor_id
 join film ON film_actor.film_id = film.film_id
 join film_category ON film.film_id = film_category.film_id
 join category ON film_category.category_id = category.category_id
 where actor.first_name = 'sandra' and actor.last_name = 'kilmer' and category.name = 'action';
+
 ///////////////////
 
