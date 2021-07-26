3fa3da578f782a845ce1cf162f850251f496ac91
Oscar Guerrero G <oguerrerog@gmail.com>
2021-07-25 23:17:55 -0400

    first commit

--- lead_Gen_Business.sql
+++ lead_Gen_Business.sql
@@ -1,5 +1,5 @@
+SET lc_time_names = 'es_ES';
 USE lead_Gen_Business;
-
 
 /* 1. ¿Qué consulta ejecutaría para obtener los ingresos totales para marzo de 2012? */
 /* /////////////// */
@@ -46,54 +46,52 @@ WHERE
 /* 4. ¿Qué consulta ejecutaría para obtener el número total de sitios 
 creados por mes por año para el cliente con una identificación de 1? 
 ¿Qué pasa con el cliente = 20? */
+/* /////////////// */
+SELECT 
+    CONCAT(clients.first_name, ' ', clients.last_name) as cliente, 
+    YEAR(sites.created_datetime) as año, 
+    MONTH(sites.created_datetime) as mes, 
+    COUNT(sites.site_id) AS total_dominios
+FROM clients
+JOIN sites ON clients.client_id = sites.client_id
+WHERE clients.client_id = 20 
+GROUP BY YEAR(sites.created_datetime), MONTH(sites.created_datetime)
+ORDER BY YEAR(sites.created_datetime), MONTH(sites.created_datetime) ASC;
+/* /////////////// */
+
+
+/* 5. ¿Qué consulta ejecutaría para obtener el número total de clientes 
+potenciales generados para cada uno de los sitios entre 
+el 1 de enero de 2011 y el 15 de febrero de 2011? */
 select * FROM sites;
 select * FROM leads;
 select * FROM clients;
 
 SELECT 
-    EXTRACT(YEAR FROM charged_datetime) AS 'Año',
-    EXTRACT(MONTH FROM charged_datetime) AS 'Mes'
-FROM billing
-JOIN clients ON billing.client_id = clients.client_id
-JOIN sites ON clients.client_id = sites.client_id
-WHERE clients.client_id = 20;
-WHERE charged_datetime >= '2012-03-01%' AND charged_datetime <= '2012-03-31%';
+    COUNT(leads.leads_id) AS '# of leads',
+    sites.domain_name,
+    leads.registered_datetime
+FROM
+    sites
+        JOIN
+    leads ON sites.site_id = leads.site_id
+        AND leads.registered_datetime >= '2011-01-01'
+        AND leads.registered_datetime <= '2011-02-15'
+GROUP BY sites.domain_name;
 
-
-SELECT * FROM billing
-    JOIN clients ON billing.client_id = clients.client_id
-    JOIN sites ON clients.client_id = sites.client_id
-WHERE clients.client_id = 1;
-
-SELECT * EXTRACT(MONTH FROM "2010-06-15 09:34:21");
-select * FROM leads;
-
-AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-SELECT YEAR(charged_datetime) as año, MONTH(charged_datetime) as mes ,COUNT(*) as total_dominios
-FROM billing
-JOIN clients ON billing.client_id = clients.client_id
-JOIN sites ON clients.client_id = sites.client_id
-WHERE clients.client_id = 1
-GROUP BY YEAR(charged_datetime), MONTH(charged_datetime);
-
-
-
-/* SELECT * FROM sites WHERE cliens.client_id = 1
-
- YEAR(created_datetime) = 2011 AND MONTH(created_datetime) = %m;
-
-/* 5. ¿Qué consulta ejecutaría para obtener el número total de clientes 
-potenciales generados para cada uno de los sitios entre 
-el 1 de enero de 2011 y el 15 de febrero de 2011?
 
 6. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes 
 y el número total de clientes potenciales que hemos generado 
 para cada uno de nuestros clientes entre 
 el 1 de enero de 2011 y el 31 de diciembre de 2011?
 
+
+
 7. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes 
 y el número total de clientes potenciales que hemos generado para 
 cada cliente cada mes entre los meses 1 y 6 del año 2011?
+
+
 
 8. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes 
 y el número total de clientes potenciales que hemos generado para 
@@ -103,6 +101,7 @@ Solicite esta consulta por ID de cliente.
 Presente una segunda consulta que muestre todos los clientes, 
 los nombres del sitio y el número total de clientes potenciales generados 
 en cada sitio en todo momento.
+
 
 9. Escriba una sola consulta que recupere los ingresos totales recaudados de cada cliente para cada mes del año. Pídalo por ID de cliente.
 
